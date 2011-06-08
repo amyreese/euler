9ba3e5e18bb8d202802c335b6cfb010b1dc98591
John Reese <john@noswap.com>
2011-06-08 12:54:03 -0400

    C++ solution to problem 4

---
+++ cpp/problem4.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <vector>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	vector<int> results;
+
+	for (int i = 999; i > 900; i--)
+	{
+		for (int j = 999; j > 900; j--)
+		{
+			results.push_back(i * j);
+		}
+	}
+
+	char buffer[16];
+	int length;
+	bool match;
+
+	for (vector<int>::iterator i = results.begin(); i != results.end(); i++)
+	{
+		match = true;
+		length = sprintf(buffer, "%u", *i);
+
+		for (int k = 0; k < length; k++)
+		{
+			if (buffer[k] != buffer[length-k-1])
+			{
+				match = false;
+			}
+		}
+
+		if (match)
+		{
+			printf("%u\n", *i);
+			break;
+		}
+	}
+
+	return 0;
+}
+
