2cd0dcd7fe8097e5ab25ffd0a8b47cb0501a8282
John Reese <john@noswap.com>
2011-06-14 14:04:28 -0400

    C++ solution to problem 14

---
+++ cpp/problem14.cpp
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <map>
+using namespace std;
+
+#define uint unsigned int
+
+int main(int argc, char* argv[])
+{
+	map<uint, uint> history;
+	map<uint, uint>::iterator h;
+
+	uint longest = 0;
+	uint longest_seed = 0;
+	uint seed = 999999;
+	uint limit = 1;
+	uint n;
+	uint count;
+
+	while (seed > limit)
+	{
+		n = seed;
+		count = 1;
+
+		while (n > 1)
+		{
+			if (n % 2 == 0)
+			{
+				n = n / 2;
+			}
+			else
+			{
+				n = 3 * n + 1;
+			}
+
+			h = history.find(n);
+			if (h != history.end() and h->second > count)
+			{
+				break;
+			}
+			else
+			{
+				history[n] = count;
+			}
+
+			count++;
+		}
+
+		if (count > longest)
+		{
+			longest = count;
+			longest_seed = seed;
+		}
+
+		seed--;
+	}
+
+	printf("%u %u\n", longest_seed, longest);
+	
+	return 0;
+}
+
