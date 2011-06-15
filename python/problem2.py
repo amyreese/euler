55f6b85e0c376ba56a2ce860fd8d33011c34bc7e
John Reese <john@noswap.com>
2011-06-15 10:55:21 -0400

    Update probelm 2's generator function

--- python/problem2.py
+++ python/problem2.py
@@ -1,19 +1,16 @@
 
 def fib(size=-1):
-    def inner():
-        count = 0
-        last = 0
-        current = 1
+    count = 0
+    last = 0
+    current = 1
 
-        while size < 0 or count < size:
-            last, current = current, last + current
+    while size < 0 or count < size:
+        last, current = current, last + current
 
-            if current > 4000000:
-                break
+        if current > 4000000:
+            break
 
-            count += 1
-            yield current
-
-    return inner()
+        count += 1
+        yield current
 
 print sum((i for i in fib() if i % 2 == 0))
