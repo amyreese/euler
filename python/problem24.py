357627f1eeb9644d36dc926ac14daf58d24acd31
John Reese <john@noswap.com>
2011-06-28 10:39:26 -0400

    Brute force python solution to problem 24

---
+++ python/problem24.py
@@ -0,0 +1,22 @@
+
+limit = 10
+perms = {}
+
+def findperms(seen=None):
+    if seen is None:
+        seen = []
+    elif len(seen) >= limit:
+        s = ''.join(map(str, seen))
+        perms[s] = True
+
+    for i in range(limit):
+        if i in seen:
+            continue
+
+        findperms(seen + [i])
+
+findperms()
+perms = sorted(perms.keys())
+
+if len(perms) > 1000000:
+    print perms[999999]
